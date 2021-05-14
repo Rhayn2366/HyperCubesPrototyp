1506c44ff6b0c56c23d1974db335e17b6ca7d26a
Oliver <oliverkreutz@hotmail.com>
2021-05-14 22:58:41 +0200

    Added new hyper cube functionalities
    ADDED color cube
    ADDED rotation cube

---
+++ Assets/Scripts/HyperCubes/CubeLogic/ColorLemmingCube.cs
@@ -0,0 +1,18 @@
+﻿using HypercubesPrototyp.GameLogic;
+using UnityEngine;
+
+namespace HypercubesPrototyp.HyperCubeLogic
+{
+    [CreateAssetMenu(menuName = "CubeLogic/ColorLemmingCube")]
+    public class ColorLemmingCube : CubeLogic
+    {
+        [SerializeField] private Color _color = Color.red;
+
+        public override void DoCommand(Lemming lemming, HyperCube hyperCube)
+        {
+            if (!lemming) return;
+            lemming.SetColor(_color);
+            lemming.SetLastUsedGameObjectAndInit(hyperCube.gameObject);
+        }
+    }
+}
---
+++ Assets/Scripts/HyperCubes/CubeLogic/YRotationCube.cs
@@ -0,0 +1,18 @@
+﻿using HypercubesPrototyp.GameLogic;
+using UnityEngine;
+
+namespace HypercubesPrototyp.HyperCubeLogic
+{
+    [CreateAssetMenu(menuName = "CubeLogic/YRotationCube")]
+    public class YRotationCube : CubeLogic
+    {
+        [SerializeField] private float _rotationAngle = 45f;
+
+        public override void DoCommand(Lemming lemming, HyperCube hyperCube)
+        {
+            if (!lemming) return;
+            lemming.SetRotationOnY(_rotationAngle);
+            lemming.SetLastUsedGameObjectAndInit(hyperCube.gameObject);
+        }
+    }
+}

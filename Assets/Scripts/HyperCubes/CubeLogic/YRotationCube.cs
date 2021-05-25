193b1df26b18ba7ceb6eff5b3c249d852865b443
Diana Engels <dianapetrich@gmail.com>
2021-05-25 12:26:21 +0200

    Set lemming position same as YRotationCube position when colliding

--- Assets/Scripts/HyperCubes/CubeLogic/YRotationCube.cs
+++ Assets/Scripts/HyperCubes/CubeLogic/YRotationCube.cs
@@ -10,6 +10,7 @@ namespace HypercubesPrototyp.HyperCubeLogic
         public override void DoCommand(Lemming lemming, HyperCube hyperCube)
         {
             if (!lemming) return;
+            lemming.transform.position = hyperCube.transform.position + Vector3.up * lemming.GetYOffset();
             lemming.AddRotationOnY(_rotationAngle);
             lemming.SetLastUsedGameObjectAndInit(hyperCube.gameObject);
         }
